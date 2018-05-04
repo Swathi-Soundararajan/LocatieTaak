e4dd74d3302423b5b01647eab01e7985fafa73bf
Swathi-Soundararajan <34891940+Swathi-Soundararajan@users.noreply.github.com>
2018-05-04 21:57:21 +0530

    Minor changes

--- app/src/main/java/com/example/user/locatietaak/GeofenceTransitionsIntentService.java
+++ app/src/main/java/com/example/user/locatietaak/GeofenceTransitionsIntentService.java
@@ -12,6 +12,8 @@ import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
@@ -29,11 +31,14 @@ import com.google.android.gms.location.places.Places;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
+import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
+
+import static android.app.Notification.VISIBILITY_PUBLIC;
 
 /**
  * Created by user on 02-04-2018.
@@ -45,6 +50,7 @@ public class GeofenceTransitionsIntentService extends IntentService {
 
     private GeoDataClient mGeoDataClient;
     String geofenceTransitionString;
+    //public String send;
 
 
 
@@ -63,39 +69,11 @@ public class GeofenceTransitionsIntentService extends IntentService {
 
         // Get the transition type.
         int geofenceTransition = geofencingEvent.getGeofenceTransition();
-        /*if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
-            showNotification("Entered:","Entering the location");
-        }
-        else if(geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
-            showNotification("Exited", "Exited the Location");
-        }else showNotification("Error","Error in location");
-    }
 
-    public void showNotification(String text, String bigText) {
-
-        // 1. Create a NotificationManager
-        NotificationManager notificationManager =
-                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        // 2. Create a PendingIntent for AllGeofencesActivity
-        Intent intent = new Intent(this, ReminderActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingNotificationIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        // 3. Create and send a notification
-        Notification notification = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle(getString(R.string.app_name))
-                .setContentText("Dont forget to complete your work")
-                .setContentIntent(pendingNotificationIntent)
-                .setStyle(new NotificationCompat.BigTextStyle().bigText(bigText))
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setAutoCancel(true)
-                .build();
-        notificationManager.notify(0, notification);*/
 
         // Test that the reported transition was of interest.
-        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER || geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER ||
+                geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
 
             // Get the geofences that were triggered. A single event can trigger multiple geofences.
             List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
@@ -184,128 +162,49 @@ public class GeofenceTransitionsIntentService extends IntentService {
         }
     }
     private void notifyLocationAlert(String locTransitionType, String locationDetails) {
+        long[] pattern = {1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000};
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingNotificationIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("lOCATION ALERT")
+                .setContentText(locationDetails)
+                .setContentIntent(pendingNotificationIntent)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText("LOC"))
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(false)
+                .setOnlyAlertOnce(true)
+                .setSound(alarmSound)
+                .setVisibility(VISIBILITY_PUBLIC)
+                .setVibrate(pattern)
+                .build();
+        notificationManager.notify(0, notification);
 
-        String CHANNEL_ID = "Locatie Taak";
+       /* String CHANNEL_ID = "Locatie Taak";
         NotificationCompat.Builder builder =
                 new NotificationCompat.Builder(this, CHANNEL_ID)
                         .setSmallIcon(R.drawable.ic_launcher_foreground)
                         .setContentTitle(locTransitionType + "Dont forget your work")
-                        .setContentText(locationDetails);
+                        .setContentText(locationDetails)
+                        .setContentIntent(pendingNotificationIntent)
+                        .setStyle(new NotificationCompat.BigTextStyle().bigText("LOC"))
+                        .setPriority(NotificationCompat.PRIORITY_HIGH)
+                        .setAutoCancel(false)
+                        .setOnlyAlertOnce(true)
+                        .setSound(alarmSound)
+                        .setVisibility(VISIBILITY_PUBLIC)
+                        .setVibrate(pattern)
+                        .build();
+        notificationManager.notify(0, notification);;
 
         builder.setAutoCancel(true);
 
         NotificationManager mNotificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        mNotificationManager.notify(0, builder.build());
+        mNotificationManager.notify(0, builder.build());*/
     }
-
-
-            /*mGeoDataClient = Places.getGeoDataClient(this, null);
-
-            mGeoDataClient.getPlaceById(geofenceTransitionDetails).addOnCompleteListener(new OnCompleteListener<PlaceBufferResponse>() {
-                @SuppressLint("RestrictedApi")
-                @Override
-                public void onComplete(@NonNull Task<PlaceBufferResponse> task) {
-                    if (task.isSuccessful()) {
-                        PlaceBufferResponse places = task.getResult();
-                        Place myPlace = places.get(0);
-                        Log.i(TAG, "Place found: " + myPlace.getName());
-                        CharSequence name = myPlace.getName();
-                        String placeName = name.toString();
-                        places.release();
-
-                        Calendar currentTime = Calendar.getInstance();
-                        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss");
-                        String formattedDate = df.format(currentTime.getTime());
-
-                        sendNotification(geofenceTransitionString + ": " + placeName + " " + formattedDate);
-                    } else {
-                        Log.e(TAG, "Place not found.");
-                    }
-                }
-            });
-            Log.i(TAG, geofenceTransitionDetails);
-        } else {
-            // Log the error.
-            Log.e(TAG, getString(R.string.geofence_transition_invalid_type, geofenceTransition));
-        }
-    }
-
-    private String getGeofenceTransitionDetails(
-            int geofenceTransition,
-            List<Geofence> triggeringGeofences) {
-
-        geofenceTransitionString = getTransitionString(geofenceTransition);
-
-
-        // Get the Ids of each geofence that was triggered.
-        ArrayList<String> triggeringGeofencesIdsList = new ArrayList<>();
-        for (Geofence geofence : triggeringGeofences) {
-            triggeringGeofencesIdsList.add(geofence.getRequestId());
-
-
-        }
-        String triggeringGeofencesIdsString = TextUtils.join(", ",  triggeringGeofencesIdsList);
-
-
-
-        return triggeringGeofencesIdsString;
-    }
-
-    private String getTransitionString(int transitionType) {
-        switch (transitionType) {
-            case Geofence.GEOFENCE_TRANSITION_ENTER:
-                return getString(R.string.geofence_transition_entered);
-            case Geofence.GEOFENCE_TRANSITION_EXIT:
-                return getString(R.string.geofence_transition_exited);
-            default:
-                return getString(R.string.unknown_geofence_transition);
-        }
-    }
-
-    private void sendNotification(String notificationDetails) {
-        // Create an explicit content Intent that starts the main Activity.
-        Intent notificationIntent = new Intent(getApplicationContext(), ReminderActivity.class);
-
-        // Construct a task stack.
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-
-        // Add the main Activity to the task stack as the parent.
-        stackBuilder.addParentStack(MainActivity.class);
-
-        // Push the content Intent onto the stack.
-        stackBuilder.addNextIntent(notificationIntent);
-
-        // Get a PendingIntent containing the entire back stack.
-        PendingIntent notificationPendingIntent =
-                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        // Get a notification builder that's compatible with platform versions >= 4
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-
-        // Define the notification settings.
-        builder.setSmallIcon(R.mipmap.ic_launcher)
-                // In a real app, you may want to use a library like Volley
-                // to decode the Bitmap.
-                .setLargeIcon(BitmapFactory.decodeResource(getResources(),
-                        R.mipmap.ic_launcher))
-                .setColor(Color.RED)
-                .setContentTitle(notificationDetails)
-                .setContentText(getString(R.string.geofence_transition_notification_text))
-                .setContentIntent(notificationPendingIntent);
-
-        // Dismiss notification once the user touches it.
-        builder.setAutoCancel(true);
-
-        // Get an instance of the Notification manager
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        // Issue the notification
-        mNotificationManager.notify(0, builder.build());
-    }*/
-
-
-
 }
--- app/src/main/java/com/example/user/locatietaak/PlaceListAdapter.java
+++ app/src/main/java/com/example/user/locatietaak/PlaceListAdapter.java
@@ -1,5 +1,6 @@
 package com.example.user.locatietaak;
 
+import android.content.ClipData;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
